7e231e2375766ac28c8196f7c00f5aecf55e7eac
mickeyyo55b <mickeyyo55b@gmail.com>
2025-04-22 16:29:25 +0800

    04221629

--- BradJava/src/tw/brad/apis/Scooter.java
+++ BradJava/src/tw/brad/apis/Scooter.java
@@ -1,6 +1,9 @@
 package tw.brad.apis;
 
 public class Scooter extends Bike{
+	public Scooter() {
+		System.out.println("Scooter()");
+	}
 	public void upSpeed() {
 		speed = speed < 1 ? 2 : speed *1.8;
 	}
