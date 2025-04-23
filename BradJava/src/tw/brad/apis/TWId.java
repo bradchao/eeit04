d25032ac26233c3452acaa6d65175e3fa776bd30
mickeyyo55b <mickeyyo55b@gmail.com>
2025-04-23 14:40:14 +0800

    04231440

--- BradJava/src/tw/brad/apis/TWId.java
+++ BradJava/src/tw/brad/apis/TWId.java
@@ -3,8 +3,24 @@ package tw.brad.apis;
 public class TWId {
 
 	public static boolean checkTWId(String id) {
-		System.out.println("OK");
-		return true;
+		boolean isRight = false;
+		/*
+		String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		if (id.length() == 10) {
+			char c1 = id.charAt(0);
+			if (letters.indexOf(c1) != -1) {
+				if (id.charAt(1)=='1' || id.charAt(1)=='2') {
+					
+				}
+			}
+		}
+		*/
+		
+		if (id.matches("[A-Z][12][0-9]{8}")) {
+			isRight = true;
+		}
+		
+		return isRight;
 	}
 	
 }
--- BradJava/src/tw/brad/tutor/Brad25.java
+++ BradJava/src/tw/brad/tutor/Brad25.java
@@ -5,7 +5,11 @@ import tw.brad.apis.TWId;
 public class Brad25 {
 
 	public static void main(String[] args) {
-		TWId.checkTWId("123");
+		if (TWId.checkTWId("A123456789")) {
+			System.out.println("OK");
+		}else {
+			System.out.println("XX");
+		}
 	}
 
 }
