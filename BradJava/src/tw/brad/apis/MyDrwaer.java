e0dccd9b0f675ed52ffbc278f7f17148f31854fb
mickeyyo55b <mickeyyo55b@gmail.com>
2025-04-28 14:36:56 +0800

    04281436

--- BradJava/src/tw/brad/apis/MyDrwaer.java
+++ BradJava/src/tw/brad/apis/MyDrwaer.java
@@ -1,11 +1,54 @@
 package tw.brad.apis;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
+import javax.swing.DebugGraphics;
 import javax.swing.JPanel;
 
 public class MyDrwaer extends JPanel{
+	private ArrayList<Point> line;
+	
+	
 	public MyDrwaer() {
 		setBackground(Color.YELLOW);
+		
+		line = new ArrayList<Point>();
+		
+		MyListener myListener = new MyListener();
+		addMouseListener(myListener);
+		addMouseMotionListener(myListener);
 	}
+	
+	private class MyListener extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			System.out.println(String.format("Press: %d x %d", e.getX(), e.getY()));
+		}
+		
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			System.out.println(String.format("Drag: %d x %d", e.getX(), e.getY()));
+		}
+	}
+	
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D)g;
+		
+		g2d.setStroke(new BasicStroke(10));
+		g2d.setColor(Color.RED);
+		g2d.drawLine(0, 0, 200, 400);
+		
+		
+	}
+	
 }
