99d85cad203370d9944820aadb8c62d51aee2615
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-05 14:33:32 +0800

    05051433

---
+++ BradJava/src/tw/brad/apis/MyPanel.java
@@ -0,0 +1,14 @@
+package tw.brad.apis;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class MyPanel extends JPanel{
+	public MyPanel() {
+		setBackground(Color.YELLOW);
+	}
+	
+	
+
+}
