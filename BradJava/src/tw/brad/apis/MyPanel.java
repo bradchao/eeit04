30eb9efd20c277112f0b760898f4fd6be0677b01
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-05 14:41:29 +0800

    05051441

--- BradJava/src/tw/brad/apis/MyPanel.java
+++ BradJava/src/tw/brad/apis/MyPanel.java
@@ -1,7 +1,12 @@
 package tw.brad.apis;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel{
@@ -9,6 +14,17 @@ public class MyPanel extends JPanel{
 		setBackground(Color.YELLOW);
 	}
 	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		try {
+			BufferedImage img = ImageIO.read(new File("dir2/ball.png"));
+			g.drawImage(img, 0, 0, null);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
 	
 
 }
