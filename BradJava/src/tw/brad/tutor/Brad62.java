868dab82b8f06ac5dd3f1ed4df3e8fc2024732a1
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-05 10:31:21 +0800

    05051031

--- BradJava/src/tw/brad/tutor/Brad62.java
+++ BradJava/src/tw/brad/tutor/Brad62.java
@@ -5,8 +5,11 @@ public class Brad62 {
 		System.out.println("main start");
 		Brad621 obj1 = new Brad621("A");
 		Brad621 obj2 = new Brad621("B");
+		Brad622 obj3 = new Brad622("C");
+		Thread mt1 = new Thread(obj3);
 		obj1.start();
 		obj2.start();
+		mt1.start();
 		System.out.println("main finish");
 	}
 }
@@ -27,3 +30,22 @@ class Brad621 extends Thread {
 		System.out.println("Brad621:m1()");
 	}
 }
+
+class Brad622 implements Runnable {
+	private String name;
+	Brad622(String name){this.name = name;}
+	public void run() {
+		for (int i=0; i<10; i++) {
+			System.out.printf("%s : %d\n", name, i);
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+	public void m1() {
+		System.out.println("Brad621:m1()");
+	}
+	
+}
+
