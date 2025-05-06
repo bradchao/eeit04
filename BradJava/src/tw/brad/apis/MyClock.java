41b5ae82ca9930698abb1f5e94ed13b96f1f0b44
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-06 10:02:03 +0800

    05061002

--- BradJava/src/tw/brad/apis/MyClock.java
+++ BradJava/src/tw/brad/apis/MyClock.java
@@ -1,9 +1,28 @@
 package tw.brad.apis;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.swing.JLabel;
 
-public class MyClock extends JLabel{
+public class MyClock extends JLabel {
+	private Timer timer;
+	private DateTimeFormatter formater;
+	
 	public MyClock() {
-		setText("16:21:10");
+		formater = DateTimeFormatter.ofPattern("HH:mm:ss");		
+		
+		timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				LocalTime time = LocalTime.now();
+				String strDatetime = time.format(formater);
+				setText(strDatetime);	
+			}
+		}, 0, 100);
 	}
+	
 }
