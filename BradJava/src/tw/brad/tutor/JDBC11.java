8370529827d60a3d2b13cc750d8f3a6462cdc9d7
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-07 15:14:31 +0800

    05071514

--- BradJava/src/tw/brad/tutor/JDBC11.java
+++ BradJava/src/tw/brad/tutor/JDBC11.java
@@ -2,15 +2,20 @@ package tw.brad.tutor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 import java.util.Scanner;
+
+import tw.brad.apis.BCrypt;
 
 public class JDBC11 {
 	private static final String URL = "jdbc:mysql://localhost/brad"; 
 	private static final String USER = "root";
 	private static final String PASSWD = "root";
+	private static Connection conn;
 
 	public static void main(String[] args) {
 		Scanner scaner = new Scanner(System.in);
@@ -26,17 +31,17 @@ public class JDBC11 {
 		prop.put("user", USER);
 		prop.put("password", PASSWD);
 		try {
-			Connection conn = DriverManager.getConnection(URL, prop);
+			conn = DriverManager.getConnection(URL, prop);
 			// check account
-			if (isAccountExist(account)) {
+			if (!isAccountExist(account)) {
 				// insert
 				try {
 					registerAccount(account, passwd, cname);
 				}catch(Exception e) {
-					
+					System.out.println(e);
 				}
 			}else {
-				
+				System.out.println("Account EXIST!");
 			}
 		}catch(Exception e) {
 			System.out.println(e);
@@ -44,6 +49,17 @@ public class JDBC11 {
 	}
 	
 	private static boolean isAccountExist(String account) {
+		String sql = "SELECT count(account) count FROM member WHERE account = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, account);
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			return rs.getInt("count") > 0;
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		
 		return false;
 	}
 	
@@ -52,6 +68,15 @@ public class JDBC11 {
 				String passwd, 
 				String cname) 
 			throws Exception{
+		String sql = "INSERT INTO member (account,passwd,cname) VALUES (?,?,?)";
+		String hashPasswd = BCrypt.hashpw(passwd, BCrypt.gensalt());
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, account);
+		pstmt.setString(2, hashPasswd);
+		pstmt.setString(3, cname);
+		if (pstmt.executeUpdate() == 0) {
+			throw new Exception("Update Failure");
+		}
 		
 	}
 
