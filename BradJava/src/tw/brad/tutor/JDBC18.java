a367e03dd9a5f613c3a0337d4c7ac00e5699bc45
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-08 12:01:02 +0800

    05081200

--- BradJava/src/tw/brad/tutor/JDBC18.java
+++ BradJava/src/tw/brad/tutor/JDBC18.java
@@ -19,8 +19,12 @@ public class JDBC18 {
 		prop.put("password", PASSWD);
 		try {
 			Connection conn = DriverManager.getConnection(URL, prop);
-			PreparedStatement pstmt = conn.prepareStatement(QUERY);
+			PreparedStatement pstmt = 
+				conn.prepareStatement(QUERY,
+						ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_UPDATABLE);
 			ResultSet rs = pstmt.executeQuery();
+			// beforeFist
 			rs.next();
 			String name = rs.getString("name");
 			System.out.println(name);
@@ -31,6 +35,53 @@ public class JDBC18 {
 			}else {
 				System.out.println("XX");
 			}
+			
+			rs.first();
+			name = rs.getString("name");
+			System.out.println(name);
+			
+			rs.last();
+			name = rs.getString("name");
+			System.out.println(name);
+			
+			if (rs.next()) {
+				System.out.println("OK");
+			}else {
+				System.out.println("XX");
+			}
+			
+			rs.absolute(147);
+			name = rs.getString("name");
+			System.out.println(name);
+			
+			rs.updateString("tel", "0912-123456");
+			rs.updateString("name", "麻辣小魚分享包V2");
+			rs.updateRow();
+			
+			
+			rs.absolute(7);
+			name = rs.getString("name");
+			System.out.println(name);
+			
+			rs.deleteRow();
+			
+			//--------------------
+			rs.moveToInsertRow();
+			rs.updateString("tel", "0912-123456");
+			rs.updateString("name", "麻辣小魚分享包V3");
+			rs.updateString("feature", "麻辣小魚粉好吃");
+			rs.insertRow();
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			
 		}catch(Exception e) {
