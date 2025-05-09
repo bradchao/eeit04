109ba7343bc631abfab3f94b0495dfbc74299677
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-09 09:20:14 +0800

    05090920

---
+++ BradJava/src/tw/brad/apis/GiftTable.java
@@ -0,0 +1,39 @@
+package tw.brad.apis;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class GiftTable extends JTable{
+	private GiftModel model;
+	
+	public GiftTable() {
+		model = new GiftModel();
+		setModel(model);
+	}
+	
+	private class GiftModel extends DefaultTableModel {
+
+		@Override
+		public int getRowCount() {
+			return 10;
+		}
+
+		@Override
+		public int getColumnCount() {
+			return 4;
+		}
+
+		@Override
+		public Object getValueAt(int row, int column) {
+			return "Brad";
+		}
+
+		@Override
+		public void setValueAt(Object aValue, int row, int column) {
+			// TODO Auto-generated method stub
+			super.setValueAt(aValue, row, column);
+		}
+		
+	}
+	
+}
--- BradJava/src/tw/brad/tutor/MyGift.java
+++ BradJava/src/tw/brad/tutor/MyGift.java
@@ -1,11 +1,20 @@
 package tw.brad.tutor;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
+import tw.brad.apis.GiftTable;
+
 public class MyGift extends JFrame{
+	private GiftTable table;
 
 	public MyGift() {
 		super("Gift");
+		
+		setLayout(new BorderLayout());
+		table = new GiftTable();
+		add(table, BorderLayout.CENTER);
 		
 		
 		
